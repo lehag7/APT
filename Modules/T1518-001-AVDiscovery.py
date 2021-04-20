9b874051faf851f903de35c34a868cc9ea641e32
lehag7 <81866942+lehag7@users.noreply.github.com>
2021-04-19 19:03:44 -0500

    Create T1518-001-AVDiscovery.py

---
+++ Modules/T1518-001-AVDiscovery.py
@@ -0,0 +1,15 @@
+## Author: lehag
+## Code: T1518-001-AVDiscovery.py
+"""
+	Description:
+		It discovers if an antivirus is active.
+"""
+import subprocess
+import sys
+
+if __name__ == "__main__":
+	print("[+] Discovering antivirus on the system.");
+
+	## Discovers antivirus on the system
+	result = subprocess.run(["wmic", "/namespace:\\\\root\\securitycenter2", "path", "antivirusproduct"]);
+  
