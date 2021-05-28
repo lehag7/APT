b10993a547e75f66285f83942d9215451140a326
lehag7 <81866942+lehag7@users.noreply.github.com>
2021-05-28 09:07:33 -0500

    Create APT.py

---
+++ APT.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+## Author: lehag
+## Code: APT.py
+"""
+    Description:
+        This project (WIP) aims to be a framework composed of modules that allow blue teams to test their threat detection capabilities in their environments by emulating the Tactics, Techniques and Procedures (TTP) followed by an adversary based on MITRE ATT&CK.
+"""
+
+class APT:
+    if __name__ == "__main__":
+        print("""
+                 █████╗    ██████╗ ████████╗
+                ██╔══██╗   ██╔══██╗╚══██╔══╝
+                ███████║   ██████╔╝   ██║   
+                ██╔══██║   ██╔═══╝    ██║   
+                ██║  ██║██╗██║██╗     ██║   
+                ╚═╝  ╚═╝╚═╝╚═╝╚═╝     ╚═╝   
+        """);
+        print("[+] Project: Advanced Persistence Testing.");
+        print("[++] Description: This project (WIP) aims to be a framework composed of modules that allow blue teams to test their threat detection capabilities in their environments by emulating the Tactics, Techniques and Procedures (TTP) followed by an adversary based on MITRE ATT&CK.");
+        print("[++] Author: lehag\n");
+        print("[+] TACTICS");
+        print("\t1. Reconnaissance.");
+        print("\t2. Resource Development.");
+        print("\t[>] Select the Tactic you wish to test in your environment: ");
