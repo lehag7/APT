7737e769ca558a618ac70eca8ef5b697289675ad
lehag7 <81866942+lehag7@users.noreply.github.com>
2021-08-17 21:26:58 -0500

    Update APT.py

--- APT.py
+++ APT.py
@@ -5,6 +5,9 @@
     Description:
         This project (WIP) aims to be a framework composed of modules that allow blue teams to test their threat detection capabilities in their environments by emulating the Tactics, Techniques and Procedures (TTP) followed by an adversary based on MITRE ATT&CK.
 """
+import json
+import subprocess
+import sys
 
 class APT:
     if __name__ == "__main__":
@@ -14,12 +17,62 @@ class APT:
                 ███████║   ██████╔╝   ██║   
                 ██╔══██║   ██╔═══╝    ██║   
                 ██║  ██║██╗██║██╗     ██║   
-                ╚═╝  ╚═╝╚═╝╚═╝╚═╝     ╚═╝   
+                ╚═╝  ╚═╝╚═╝╚═╝╚═╝     ╚═╝   : Advanced Persistence Testing.
         """);
-        print("[+] Project: Advanced Persistence Testing.");
-        print("[++] Description: This project (WIP) aims to be a framework composed of modules that allow blue teams to test their threat detection capabilities in their environments by emulating the Tactics, Techniques and Procedures (TTP) followed by an adversary based on MITRE ATT&CK.");
-        print("[++] Author: lehag\n");
+        print("[+] Description: This project (WIP) aims to be a framework composed of modules that allow blue teams to test their threat detection capabilities in their environments by emulating the Tactics, Techniques and Procedures (TTP) followed by an adversary based on MITRE ATT&CK.");
+        print("[+] Author: lehag\n")
+
+        ## Read the Tactics
         print("[+] TACTICS");
-        print("\t1. Reconnaissance.");
-        print("\t2. Resource Development.");
-        print("\t[>] Select the Tactic you wish to test in your environment: ");
+        with open("TTP/Tactics.json") as fd:
+            tactics = json.load(fd);
+            for i in tactics["Tactic"]:
+                print("\t" + i["ID"] + " - " + i["Name"]);
+        id = input("\t[>] Select the Tactic-ID you wish to test in your environment: ");
+
+        ## Read the Techniques
+        for i in tactics["Tactic"]:
+            if i["ID"] == id:
+                break;
+        print("[+] TECHNIQUES");
+        print("\t" + i["ID"] + " - " + i["Name"]);
+        print("\tDescription: " + i["Description"] + "\n");
+        with open(i["Path"]) as fd:
+            tech = json.load(fd);
+            for i in tech["Technique"]:
+                print("\t" + i["ID"] + " - " + i["Name"]);
+        id = input("\t[>] Select the Technique-ID you wish to test in your environment: ");
+
+        ## Read the SubTechniques
+        for i in tech["Technique"]:
+            if i["ID"] == id:
+                break;
+        print("[+] SUBTECHNIQUES");
+        print("\t" + i["ID"] + " - " + i["Name"]);
+        print("\tDescription: " + i["Description"] + "\n");
+        with open(i["Path"]) as fd:
+            tech = json.load(fd);
+            for i in tech["SubTechnique"]:
+                print("\t" + i["ID"] + " - " + i["Name"]);
+        id = input("\t[>] Select the SubTechnique-ID you wish to test in your environment: ");
+
+        ## Read the Procedure
+        for i in tech["SubTechnique"]:
+            if i["ID"] == id:
+                break;
+        print("[+] PROCEDURES");
+        print("\t" + i["ID"] + " - " + i["Name"]);
+        print("\tDescription: " + i["Description"] + "\n");
+        for j in i["Procedure"]:
+            print("\t" + j["ID"] + " - " + j["Process"] + " " + j["Commandline"]);
+        id = input("\t[>] Select the Procedure-ID you wish to test in your environment: ");
+        
+        ## Execute the Procedure
+        for j in i["Procedure"]:
+            if j["ID"] == id:
+                break;
+        print("[...] Executing the TTP.");
+        process = j["Process"];
+        commandline = j["Commandline"];
+        args = process + " " + commandline; 
+        result = subprocess.run(args);
