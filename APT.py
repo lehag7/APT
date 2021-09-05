3e12765ad8a43cdc93cb9377beb8934391f6f80e
lehag7 <81866942+lehag7@users.noreply.github.com>
2021-09-05 14:55:18 -0500

    Update APT.py

--- APT.py
+++ APT.py
@@ -14,94 +14,111 @@ class APT:
     def __init__(self):
         self.TTP = "TTP.json";
         self.TTPData = "";
-        self.TacticsData = "";
         self.TacticID = "";
-        self.TechniquesData = "";
         self.TechniqueID = "";
         self.SubTechniqueID = "";
-        self.SubTechniquesData = "";
         self.ProcedureID = "";
 
+    def startAPT(self):
+        """
+            This is the main method of the A.P.T framework, it starts the remaining methods. 
+        """
+        self.displayDescription();
+        self.loadTTP();
+        
+        while(True):
+            apt.displayTactics();
+            if(self.TacticID in self.TTPData):
+                self.displayTechniques();
+                if(self.TechniqueID in self.TTPData[self.TacticID]["Technique"]):
+                    if("SubTechnique" in self.TTPData[self.TacticID]["Technique"][self.TechniqueID]):
+                        self.displaySubTechniques();
+                        if(self.SubTechniqueID in self.TTPData[self.TacticID]["Technique"][self.TechniqueID]["SubTechnique"]):
+                            self.displayProcedures();
+                        elif(self.SubTechniqueID == "E"):
+                            print("Bye!");
+                            exit(0);
+                        else:
+                            print("\t[-] SubTechnique ID not found");
+                            continue;
+                    else:
+                        self.displayProcedures();
+                elif(self.TechniqueID == "E"):
+                    print("Bye!");
+                    exit(0);
+                else:
+                    print("\t[-] Technique ID not found");
+                    continue;
+            elif(self.TacticID == "E"):
+                print("Bye!");
+                exit(0);
+            else:
+                print("\t[-] Tactic ID not found");
+                continue;
+
     def displayDescription(self):
+        """
+            This method displays the description of the A.P.T framework.
+        """
         print("""
             ╔═╗┌┬┐┬  ┬┌─┐┌┐┌┌─┐┌─┐┌┬┐  ╔═╗┌─┐┬─┐┌─┐┬┌─┐┌┬┐┌─┐┌┐┌┌─┐┌─┐  ╔╦╗┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
             ╠═╣ ││└┐┌┘├─┤││││  ├┤  ││  ╠═╝├┤ ├┬┘└─┐│└─┐ │ ├┤ ││││  ├┤    ║ ├┤ └─┐ │ │││││ ┬
             ╩ ╩─┴┘ └┘ ┴ ┴┘└┘└─┘└─┘─┴┘  ╩  └─┘┴└─└─┘┴└─┘ ┴ └─┘┘└┘└─┘└─┘   ╩ └─┘└─┘ ┴ ┴┘└┘└─┘
         """);
         print("[+] Description: This project (WIP) aims to be a framework composed of modules that allow blue teams to test their threat detection capabilities in their environments by emulating the Tactics, Techniques and Procedures (TTP) followed by an adversary based on MITRE ATT&CK.");
-        print("[+] Author: lehag\n");
-    
-    def loadTactics(self):
+        print("[+] Author: @lehag07\n");
+
+    def loadTTP(self):
+        """
+            This method loads the Tactics, Techniques and Procedures stored in the file "TTP.json". 
+        """
+        try:
+            with open(self.TTP) as fd:
+                self.TTPData = json.load(fd);
+        except Exception as e:
+            print("\tError loading file 'TTP.json': " + str(e));
+            exit(0);
+
+    def displayTactics(self):
+        """
+            This method displays the Tactics available in the A.P.T framework.
+        """
         print("[+] TACTICS");
-        with open(self.TTP) as fd:
-            self.TTPData = json.load(fd);
-        
         for id in self.TTPData:
             print("\t" + id + " : " + self.TTPData[id]["Name"]);
         print("\tE - Exit");
         self.TacticID = input("\n\tTTP> ");
-        return self.TacticID;
 
-    def loadTechniques(self, TacticID):
+    def displayTechniques(self):
+        """
+            This method displays the Techniques available in the A.P.T framework.
+        """
         print("[+] TECHNIQUES");
-
         for id in self.TTPData[self.TacticID]["Technique"]:
             print("\t" + id + " : " + self.TTPData[self.TacticID]["Technique"][id]["Name"]);
         print("\tE - Exit");
         self.TechniqueID = input("\n\tTTP>" + self.TacticID + ">");
-        return self.TechniqueID;
 
-    def loadSubTechniques(self, TechniqueID):
-        for i in self.TechniquesData["Technique"]:
-            if i["ID"] == self.TechniqueID:
-                break;
+    def displaySubTechniques(self):
+        """
+            This method displays the SubTechniques available in the A.P.T framework.
+        """
         print("[+] SUBTECHNIQUES");
-        for i in self.TacticsData["Tactic"]:
-            if(i["ID"] == TacticID):
-                for j in i["Technique"]:
-                    if(j["ID"] == TechniqueID):
-                        for k in j["Subtechnique"]:
-                            print("\t" + j["ID"] + " - " + j["Name"]);
+        for id in self.TTPData[self.TacticID]["Technique"][self.TechniqueID]["SubTechnique"]:
+            print("\t" + id + " : " + self.TTPData[self.TacticID]["Technique"][self.TechniqueID]["SubTechnique"][id]["Name"]);
+        print("\tE - Exit");
+        self.SubTechniqueID = input("\n\tTTP>" + self.TacticID + ">" + self.TechniqueID + ">");
 
-
-        with open(i["Path"]) as fd:
-            self.SubTechniquesData = json.load(fd);
-            for i in self.SubTechniquesData["SubTechnique"]:
-                print("\t" + i["ID"] + " - " + i["Name"]);
-        self.SubTechniqueID = input("\n\tTTP>" + self.TacticID + ">" + self.TechniqueID + "> Select the SubTechnique-ID: ");
-
-    def loadProcedures(self):
-        for i in self.SubTechniquesData["SubTechnique"]:
-            if i["ID"] == id:
-                break;
+    def displayProcedures(self):
+        """
+            This method displays the Procedures available in the A.P.T framework.
+        """
         print("[+] PROCEDURES");
-        for j in i["Procedure"]:
-            print("\t" + j["ID"] + " - " + j["Process"] + " " + j["Commandline"]);
-        self.ProcedureID = input("\n\tTTP>" + self.TacticID + ">" + self.TechniqueID + ">" + self.SubTechniqueID + "> Select the Procedure-ID: ");
-
-        for j in i["Procedure"]:
-            if j["ID"] == self.ProcedureID:
-                break;
-        print("[...] Executing the TTP.");
-        process = j["Process"];
-        commandline = j["Commandline"];
-        args = process + " " + commandline; 
-        result = subprocess.run(args);
+        for id in self.TTPData[self.TacticID]["Technique"][self.TechniqueID]["SubTechnique"][self.SubTechniqueID]["Procedure"]:
+            print("\t" + id + " : " + self.TTPData[self.TacticID]["Technique"][self.TechniqueID]["SubTechnique"][self.SubTechniqueID]["Procedure"][id]);
+        print("\tE - Exit");
+        self.ProcedureID = input("\n\tTTP>" + self.TacticID + ">" + self.TechniqueID + ">" + self.SubTechniqueID + ">");
 
 if __name__ == "__main__":
     apt = APT();
-    apt.displayDescription();
-    while(True):
-        TacticID = apt.loadTactics();
-        if(TacticID != "E"):
-            TechniqueID = apt.loadTechniques(TacticID);
-            exit(0);
-            if(TechniqueID != "E"):
-                SubTechniqueID = 3;
-                if(SubTechniqueID == None):
-                    apt.loadSubTechniques();
-                    apt.loadProcedures();
-            else:
-                exit(0);
-        else:
-            exit(0);
+    apt.startAPT();
